3f4c43e24d73dbdc6b374e270c264e8893384ac6
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-09-17 18:02:43 +0300

    typos

--- DutyWallet/DutyWallet/UI/Screen/View/MainView.swift
+++ DutyWallet/DutyWallet/UI/Screen/View/MainView.swift
@@ -11,11 +11,11 @@ struct MainView: View {
     @StateObject var mainVM = MainVewModel(repository: RealmRepository<Duty>())
     @State private var addAlert = false
     
-    struct CreditDutyView: View {
-        var duties: [Duty]
-        
+    struct CreditOrDutyView: View {
         @State private var name = ""
         @State private var amount = ""
+        
+        var duties: [Duty]
         var role: Person.Role
         var deleteCell: (IndexSet, Person.Role) -> ()
         var add: (String, String, Person.Role) -> ()
@@ -44,16 +44,16 @@ struct MainView: View {
             .alert("Новый долг",
                    isPresented: $alert,
                    actions: {
-                TextField(role == .creditor ? "Кто мне будет должен" : "Кому я буду должен", text: $name)
-                    TextField("Сумма", text: $amount)
-#if os(iOS)
-                        .keyboardType(.numberPad)
-#endif
-                    Button("Добавить", action: {
-                        add(amount, name, role)
-                        name = ""; amount = ""
-                    })
-                Button("Отмена", role: .cancel, action: { name = ""; amount = "" })
+                    TextField(role == .creditor ? "Кто мне будет должен" : "Кому я буду должен", text: $name)
+                        TextField("Сумма", text: $amount)
+    #if os(iOS)
+                            .keyboardType(.numberPad)
+    #endif
+                        Button("Добавить", action: {
+                            add(amount, name, role)
+                            name = ""; amount = ""
+                        })
+                    Button("Отмена", role: .cancel, action: { name = ""; amount = "" })
                 }
             )
         }
@@ -62,13 +62,13 @@ struct MainView: View {
     var body: some View {
         NavigationStack {
             TabView {
-                CreditDutyView(duties: mainVM.creditors, role: .creditor, deleteCell: mainVM.deleteDuty, add: mainVM.addDuty, alert: $addAlert)
+                CreditOrDutyView(duties: mainVM.creditors, role: .creditor, deleteCell: mainVM.deleteDuty, add: mainVM.addDuty, alert: $addAlert)
                 .tabItem {
                     Image(systemName: "square.and.arrow.down")
                     Text("Мне должны")
                 }
                 
-                CreditDutyView(duties: mainVM.debtors, role: .debtor, deleteCell: mainVM.deleteDuty, add: mainVM.addDuty, alert: $addAlert)
+                CreditOrDutyView(duties: mainVM.debtors, role: .debtor, deleteCell: mainVM.deleteDuty, add: mainVM.addDuty, alert: $addAlert)
                 .tabItem {
                     Image(systemName: "square.and.arrow.up")
                     Text("Я должен")
@@ -87,43 +87,6 @@ struct MainView: View {
         }
     }
 }
-
-//                Group {
-//                    List {
-//                        ForEach(mainVM.debtors, id: \.id) { duty in
-//                            NavigationLink(destination: {
-//                                DetailDutyView()
-//                            }, label: {
-//                                HStack {
-//                                    Image(systemName: "person.fill")
-//                                    Text(duty.person.name)
-//                                    Spacer()
-//                                    Image(systemName: "rublesign")
-//                                    Text(duty.amount.description)
-//                                }
-//                            })
-//                            .padding()
-//                        }
-//                        .onDelete { indexSet in
-//                            mainVM.deleteDuty(at: indexSet, inCreditors: false)
-//                        }
-//                    }
-//                    .alert("Новый кредит",
-//                           isPresented: $alert,
-//                           actions: {
-//                            TextField("Кому я буду должен", text: $name)
-//                            TextField("Сумма", text: $amount)
-//                            #if os(iOS)
-//                                .keyboardType(.numberPad)
-//                            #endif
-//                            Button("Добавить", action: {
-//                                mainVM.addDuty(amount: amount, name: name, role: .debtor)
-//                                name = ""; amount = ""
-//                            })
-//                            Button("Отмена", role: .cancel, action: { name = ""; amount = "" })
-//                        }
-//                    )
-//                }
 
 #Preview {
     MainView()
